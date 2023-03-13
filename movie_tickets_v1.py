85dbc7f9de3fb51f18f4440787ee4d252f37572a
eleashac <124128668+eleashac@users.noreply.github.com>
2023-03-14 11:10:06 +1300

    Update movie_tickets_v1.py

--- movie_tickets_v1.py
+++ movie_tickets_v1.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+"""
+Movie theatre ticketing system - v1
+Welcome screen and set up variables
+Created by Eleasha Chan
+"""
+
+
+# Component 1 - welcome screen and set up variables
+def sell_ticket():
+    print("********** Fanfare Movies - ticketing system **********\n")
+
+    adult_tickets = 0
+    child_tickets = 0
+    student_tickets = 0
+    gift_tickets = 0
+    total_sales = 0
+    tickets_sold = 0
+
+
+# Main routines
+sell_ticket()
